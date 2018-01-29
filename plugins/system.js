ecee19d838cd80edf08cfc97053369a6d091fac7
Tonk1e <ttt0nk1e@gmail.com>
2018-01-29 17:34:22 +0000

    added a functioning system info embed

--- plugins/system.js
+++ plugins/system.js
@@ -1,6 +1,7 @@
 // discord
 const discord = require('discord.js')
-const bot = new discord.Client()
+const mainClass = require('../mainClass.js')
+const bot = mainClass.bot
 
 // other requirements
 const os = require('os')
@@ -9,22 +10,28 @@ const os = require('os')
 class System{
 	constructor(){
 		this.cpuArray = os.cpus()
-		this.cpuModel = this.cpuArray.model
+		this.cpuModel = this.cpuArray[0].model
 		this.cpuArchitecture = os.arch()
+		this.cpuSpeed = this.cpuArray[0].speed / 1000
 		this.freeMemB = os.freemem()
 		this.freeMemGB = Math.floor(os.freemem()/1073741824)
+		this.sysUptime = os.uptime()
 	}
 }
 
 const sys = new System()
 
 var getSysEmbed = () =>{
+	console.log(os.networkInterfaces())
+	console.log(sys.cpuArray)
 	embed = new discord.RichEmbed()
 	embed.setTitle('System Info')
+	embed.setThumbnail(bot.user.avatarURL)
 	embed.setColor('ORANGE')
 	embed.addField('CPU Model', sys.cpuModel)
 	embed.addField('Architecture', sys.cpuArchitecture)
-	embed.addField('Free Memory (B)', sys.freeMemB + ' Bytes')
+	embed.addField('Speed', sys.cpuSpeed + 'GHz')
+	embed.addField('Operating System', os.type())
 	embed.addField('Free Memory', sys.freeMemGB + ' GB' + ' (Floored)')
 	return embed
 }
