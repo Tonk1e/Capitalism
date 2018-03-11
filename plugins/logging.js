5c897a2c2c6ee9c291bafd56b7474375dfbce1e5
Tonk1e <ttt0nk1e@gmail.com>
2018-03-11 18:05:39 +0000

    added a method for returning the logging file

--- plugins/logging.js
+++ plugins/logging.js
@@ -4,6 +4,53 @@ const mainClass = require('../mainClass.js')
 const bot = mainClass.bot
 
 // main
-var loggingHandler = (func,) =>{
+var randID = () =>{
+  	var text = ""
+  	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+  	for (var i = 0; i < 5; i++){
+		text += possible.charAt(Math.floor(Math.random() * possible.length))
+  	}
+
+	return text
 }
+
+var returnLogs = (x, flag) =>{
+	switch(flag){
+		case '-h':{
+			embed = new discord.RichEmbed()
+			embed.setTitle("Logs Help")
+			embed.setColor('ORANGE')
+			flags = ['', '-h', '-c']
+			flagUse = ['Returns the log file.', 'Returns a log help embed.', 'Sends the logs to the chat.']
+			var i
+			for(i=0;i<flags.length;i++){
+				embed.addField("/logs " + flags[i], flagUse[i])
+			}
+			x.channel.send(embed)
+			break
+		}
+		default:{
+			break
+		}
+	}
+	fs.readFile('./logs.log', (err, data) => {
+		if (err) throw err;
+	  	
+	  	x.reply("Here are the logs.")
+	  	x.channel.send({files: [{attachment: "./logs.log"}]})
+	});
+}
+
+var loggingHandler = (x, comm, func) =>{
+	fs.appendFile("./logs.log", "[command] " + randID() + ": " + x.author.username + "#" + x.author.discriminator + " @ " + x.guild.name + " >> " + comm + "\r\n", function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+    	console.log("[command] " + randID() + ": " + x.author.username + "#" + x.author.discriminator + " @ " + x.guild.name + " >> " + comm);
+	}); 
+}
+
+module.exports.loggingHandler = loggingHandler
+module.exports.returnLogs = returnLogs
--- query.js
+++ query.js
@@ -22,6 +22,7 @@ const help = require('./plugins/help.js')
 const dev = require('./plugins/dev.js')
 const vid = require('./plugins/vid.js')
 const ftp = require('./plugins/ftp.js')
+const logging = require('./plugins/logging.js')
 
 // other requirements
 const fs = require('fs')
@@ -45,6 +46,7 @@ var incrementCommandUse = (id) =>{
 var query = (x, y) =>{
 	switch(y){
 		case '/hello':{
+			logging.loggingHandler(x, '/nut', 'nut.nut(x)')
 			incrementCommandUse(x.author.id)
 			x.reply('Hello there.')
 			var getWage = money.getWage(x.author.id)
@@ -60,6 +62,7 @@ var query = (x, y) =>{
 			break
 		}
 		case '/jazz':{
+			logging.loggingHandler(x, '/jazz', 'music.jazz(x.member.voiceChannel, x)')
 			incrementCommandUse(x.author.id)
 			if(x.member.voiceChannel){
 				x.reply("Here's some jazz for you.")
@@ -70,24 +73,28 @@ var query = (x, y) =>{
 			break
 		}
 		case '/myself':{
+			logging.loggingHandler(x, '/myself', 'userInfo.getUserEmbed(x.author)')
 			incrementCommandUse(x.author.id)
 			userEmbed = userInfo.getUserEmbed(x.author)
 			x.channel.sendEmbed(userEmbed)
 			break
 		}
 		case '/wage':{
+			logging.loggingHandler(x, '/wage', 'money.getWageEmbed(x.author.id, x.author)')
 			incrementCommandUse(x.author.id)
 			wageEmbed = money.getWageEmbed(x.author.id, x.author)
 			x.channel.sendEmbed(wageEmbed)
 			break
 		}
 		case '/status':{
+			logging.loggingHandler(x, '/status', 'money.getStatusEmbed(x.author)')
 			incrementCommandUse(x.author.id)
 			statusEmbed = money.getStatusEmbed(x.author)
 			x.channel.sendEmbed(statusEmbed)
 			break
 		}
 		case '/changewage':{
+			logging.loggingHandler(x, '/changewage', 'nut.nut(x)')
 			incrementCommandUse(x.author.id)
 			if(x.author.id == '292556142952054794'){
 				id = x.content.substr(13, 18)
@@ -99,6 +106,7 @@ var query = (x, y) =>{
 			}
 		}
 		case '/sys':{
+			logging.loggingHandler(x, '/sys', 'system.getSysEmbed()')
 			incrementCommandUse(x.author.id)
 			var system = require('./plugins/system.js')
 			sysEmbed = system.getSysEmbed()
@@ -106,42 +114,50 @@ var query = (x, y) =>{
 			break
 		}
 		case '/ping':{
+			logging.loggingHandler(x, '/ping', 'ping.ping(x)')
 			incrementCommandUse(x.author.id)
 			ping.ping(x)
 			break
 		}
 		case '/avatar':{
+			logging.loggingHandler(x, '/avatar', 'userInfo.sendUserAvatar(x)')
 			incrementCommandUse(x.author.id)
 			userInfo.sendUserAvatar(x)
 			break
 		}
 		case '/setup':{
+			logging.loggingHandler(x, '/setup', 'setup.startBotSetup(x)')
 			incrementCommandUse(x.author.id)
 			setup.startBotSetup(x)
 			break
 		}
 		case '/token':{
+			logging.loggingHandler(x, '/token', 'setup.getToken(x)')
 			incrementCommandUse(x.author.id)
 			setup.getToken(x)
 			break
 		}
 		case '/register':{
+			logging.loggingHandler(x, '/register', 'money.createAccount(x)')
 			incrementCommandUse(x.author.id)
 			money.createAccount(x)
 			break
 		}
 		case '/bal':{
+			logging.loggingHandler(x, '/bal', 'money.accountEmbed(x.author)')
 			incrementCommandUse(x.author.id)
 			accEmbed = money.accountEmbed(x.author)
 			x.channel.send(accEmbed)
 			break
 		}
 		case '/check':{
+			logging.loggingHandler(x, '/check', 'money.returnCounterEmbed(x)')
 			incrementCommandUse(x.author.id)
 			money.returnCounterEmbed(x)
 			break
 		}
 		case '/bye':{
+			logging.loggingHandler(x, '/bye', 'bot.destroy()')
 			incrementCommandUse(x.author.id)
 			if(x.author.id == '292556142952054794'){
 				bot.destroy()
@@ -149,63 +165,76 @@ var query = (x, y) =>{
 			break
 		}
 		case '/reset':{
+			logging.loggingHandler(x, '/reset', 'money.economyReset(x)')
 			incrementCommandUse(x.author.id)
 			money.economyReset(x)
 			break
 		}
 		case '/shop':{
+			logging.loggingHandler(x, '/shop', 'shop.shopEmbed(x)')
 			incrementCommandUse(x.author.id)
 			shop.shopEmbed(x)
 			break
 		}
 		case '/pass':{
+			logging.loggingHandler(x, '/pass', 'games.passEmbed(x)')
 			incrementCommandUse(x.author.id)
 			games.passEmbed(x)
 			break
 		}
 		case '/politics':{
+			logging.loggingHandler(x, '/politics', 'politics.beliefEmbed(x)')
 			incrementCommandUse(x.author.id)
 			politics.beliefEmbed(x)
 			break
 		}
 		case '/beliefs':{
+			logging.loggingHandler(x, '/beliefs', 'nut.nut(x)')
 			incrementCommandUse(x.author.id)
 			politics.beliefsEmbed(x)
 			break
 		}
 		case '/parties':{
+			logging.loggingHandler(x, '/parties', 'politics.partiesEmbed(x)')
 			incrementCommandUse(x.author.id)
 			politics.partiesEmbed(x)
 			break
 		}
 		case '/referendum':{
+			logging.loggingHandler(x, '/referendum', 'politics.startReferendum(x)')
 			incrementCommandUse(x.author.id)
 			politics.startReferendum(x)
 			break
 		}
 		case '/card':{
+			logging.loggingHandler(x, '/card', 'userInfo.createProfileCard(x)')
 			card = userInfo.createProfileCard(x)
 			x.channel.send(card)
 			break
 		}
 		case '/run':{
+			logging.loggingHandler(x, '/run', 'userInfo.commandsRun(x)')
 			incrementCommandUse(x.author.id)
 			userInfo.commandsRun(x)
 			break
 		}
 		case '/markets':{
+			logging.loggingHandler(x, '/markets', 'market.returnMarkets(x)')
 			market.returnMarkets(x)
 			break
 		}
 		case '/help':{
+			logging.loggingHandler(x, '/help', 'help.sendHelp(x)')
 			help.sendHelp(x)
 			break
 		}
 		case '/update':{
+			logging.loggingHandler(x, '/update', 'dev.update(x)')
 			dev.update(x)
 			break
 		}
 		case '/nut':{
+			logging.loggingHandler(x, '/nut', 'nut.nut(x)')
 			fs.readFile('nut.txt', 'utf8', function(err, data) {  
 			    if (err) throw err;
 			    console.log(data);
@@ -222,54 +251,68 @@ var query = (x, y) =>{
 		}
 	}
 	if(y.startsWith('/transfer')){
+		logging.loggingHandler(x, '/transfer', 'money.transfer(x.author.id, id, amount)')
 		var id = y.substr(11, 28)
 		var amount = y.substr(30)
 		var transfer = money.transfer(x.author.id, id, amount)
 		x.reply(transfer)
 	}
 	if(y.startsWith('/belief') && y != "/beliefs"){
+		logging.loggingHandler(x, '/belief', 'politics.updateBelief(x, x.author.id, belief)')
 		incrementCommandUse(x.author.id)
 		belief = y.substr(8)
 		console.log(belief)
 		politics.updateBelief(x, x.author.id, belief)
 	}
 	if(y.startsWith('/party')){
+		logging.loggingHandler(x, '/party', 'politics.updateParty(x, x.author.id, party)')
 		incrementCommandUse(x.author.id)
 		party = y.substr(7)
 		console.log(party)
 		politics.updateParty(x, x.author.id, party)
 	}
 	if(y.startsWith('/pc')){
+		logging.loggingHandler(x, '/pc', 'pc.main(x)')
 		incrementCommandUse(x.author.id)
 		console.log("yay.")
 		pc.main(x)
 	}
 	if(y.startsWith('/vidinfo')){
+		logging.loggingHandler(x, '/vidinfo', 'yt.videoEmbed(x)')
 		incrementCommandUse(x.author.id)
 		yt.videoEmbed(x)
 	}
 	if(y.startsWith('/acc')){
+		logging.loggingHandler(x, '/acc', 'money.returnID(x)')
 		incrementCommandUse(x.author.id)
 		money.returnID(x)
 	}
 	if(y.startsWith('/copy')){
+		logging.loggingHandler(x, '/copy', 'random.copy(x)')
 		incrementCommandUse(x.author.id)
 		random.copy(x)
 	}
 	if(y.startsWith('/buy')){
+		logging.loggingHandler(x, '/buy', 'shop.buy(x, itemID, x.author.id)')
 		incrementCommandUse(x.author.id)
 		var itemID = parseInt(y.substr(5))
 		buyFunc = shop.buy(x, itemID, x.author.id)
 		x.reply(buyFunc)
 	}
 	if(y.startsWith('/stock')){
+		logging.loggingHandler(x, '/stock', 'market.getInstrument(x, inst)')
 		inst = y.substr(7)
 		market.getInstrument(x, inst)
 	}
 	if(y.startsWith('/ftp')){
+		logging.loggingHandler(x, '/ftp', 'ftp.handleFtpRequest(x, file)')
 		console.log(y.substr(5))
 		file = '/home/ftp/' + y.substr(5)
 		ftp.handleFtpRequest(x, file)
+	}
+	if(y.startsWith('/logs')){
+		flag = y.substr(6)
+		logging.returnLogs(x, flag)
 	}
 }
 
