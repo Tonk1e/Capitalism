9ad402a4971f5d163ace9e1bef96b23b9802a77b
Tonk1e <ttt0nk1e@gmail.com>
2018-03-14 16:23:32 +0000

    mother fricker

--- plugins/ping.js
+++ plugins/ping.js
@@ -57,6 +57,7 @@ var ping = (x) =>{
 		embed.addField('Current Machine', info["machine"])
 		embed.addField('Uptime', main.getUptime(x))
 		var currentGuilds = 0
+		currentGuilds++
 		for(var i=0;i<bot.guilds.array();i++){
 			currentGuilds++
 		}
