12096f7aa70326642b0901431144bb3a2ce1370e
Tonk1e <ttt0nk1e@gmail.com>
2018-03-25 21:41:53 +0100

    stuff

---
+++ plugins/food.js
@@ -0,0 +1,12 @@
+// discord
+const discord = require('discord.js')
+const mainClass = require('../mainClass.js')
+const bot = mainClass.bot
+
+// other requirements
+var spoon = require('spoonacular')
+
+// main
+var getRecipe(channel, search){
+	
+}
